7ee9c47524dea1eab217e2e513a20818fb3f02bf
demianov-an <Alexm92@inbox.ru>
2024-01-12 14:49:52 +0600

    2 -  Error404, Navigate

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React, {useState} from 'react';
 import styles from "./components/Site.module.css";
+import {PageOne} from './components/pages/PageOne';
+import {PageTwo} from './components/pages/PageTwo';
+import {PageThree} from './components/pages/PageThree';
+import {Navigate, Route, Routes} from 'react-router-dom';
+import {Error404} from './components/pages/Error404';
 
 
 function App() {
@@ -11,7 +16,14 @@ function App() {
                     Здесь будет навигация
                 </div>
                 <div className={styles.content}>
-                    Здесь будут кроссовки
+                  <Routes>
+                    <Route path='/' element={<Navigate to='/page1'/>}/>
+                    <Route path='/page1' element={<PageOne/>}/>
+                    <Route path='/page2' element={<PageTwo/>}/>
+                    <Route path='/page3' element={<PageThree/>}/>
+                    <Route path='/error404' element={<Error404/>}/>
+                    <Route path='/*' element={<Navigate to='/error404'/>}/>
+                  </Routes>
                 </div>
             </div>
             <div className={styles.footer}>abibas 2023</div>
